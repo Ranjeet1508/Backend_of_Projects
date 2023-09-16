15c1b9fece41566242a285d466a9af565a692d60
Ranjeet1508 <danavrj1998@gmail.com>
2023-09-16 10:52:52 +0530

    my backend

--- Routes/africaRoutes.js
+++ Routes/africaRoutes.js
@@ -33,7 +33,7 @@ africaRouter.get("/", async(req,res) => {
 
 africaRouter.get("/country/:_id", async(req,res) => {
         const {_id} = req.params;
-        //const objectId = mongoose.Types.ObjectId(_id);
+       // //const objectId = mongoose.Types.ObjectId(_id);
     try {
         const product = await AfricaModel.findById(_id)
         res.send(product)
